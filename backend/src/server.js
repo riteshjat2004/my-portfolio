70f64c36287aad1c8c5231d291750fd2e12eb031
Ritesh Jat <link4riteshjat@gmail.com>
2026-06-03 16:09:14 +0530

    Production&Deployment05

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,11 +26,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin : true,
-    // origin: [
-    //   "http://localhost:3000",
-    //   "https://your-vercel-domain.vercel.app",
-    // ],
+    origin: [
+      "http://localhost:3000",
+      "https://my-portfolio-two-theta-51.vercel.app",
+    ],
     credentials: true,
   })
 );
